bc64abce111ae79162442dfda7401d54261c86b8
Jdev <joseph.clx@gmail.com>
2022-01-03 18:58:57 +0100

    FromStringToDateTime

--- FromStringToDateTime.cs
+++ FromStringToDateTime.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Globalization;
 namespace CSharpDiscovery.Quest02
 {
     public class FromStringToDateTime_Exercice
     {
         public static DateTime FromStringToDateTime(string dateStr)
         {
-            return(System.Convert.ToDateTime(dateStr));
+            return DateTime.ParseExact(dateStr, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);  
         }
     }
 }
