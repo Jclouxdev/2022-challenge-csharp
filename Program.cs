0b566b4c0819394e7979c48964d077fe79604b46
Jdev <joseph.clx@gmail.com>
2022-01-03 22:02:23 +0100

    SecretSanta

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace TestCSharp
             var people = new HashSet<string> { "Arnaud", "Marie", "Alexandre", "Audrey", "Adrien", "Mathilde", "Théophile", "Hanako" };
             var draw = SecretSanta_Exercice.SecretSantaDraw(people);
 
+            Console.WriteLine(people.Count);
+            Console.WriteLine(draw.Count);
+
             foreach (KeyValuePair<string, string> pair in draw) {
                 Console.WriteLine("{0} is offering to {1}", pair.Key, pair.Value);
             }
--- SecretSanta.cs
+++ SecretSanta.cs
@@ -6,19 +6,21 @@ namespace CSharpDiscovery.Quest02
     {
         public static Dictionary<string,string> SecretSantaDraw(HashSet<string> people)
         {
+            // On créer un clone de "people" car sinon on modifie l'objet source "people";
+            var inerpoeple = new HashSet<string>(people);
             var dictionary = new Dictionary<string, string>();
-            while (people.Count > 0) {
+            while (inerpoeple.Count > 0) {
                 var random = new Random();
-                var gifter = random.Next(people.Count-1);
-                var iterator = people.GetEnumerator();
+                var gifter = random.Next(inerpoeple.Count-1);
+                var iterator = inerpoeple.GetEnumerator();
                 var i = 0;
                 while(iterator.MoveNext()) {
                     if(i == gifter) {
                         var gifterName = iterator.Current;
                         iterator.MoveNext();
                         var giftedName = iterator.Current;
-                        people.Remove(gifterName);
-                        people.Remove(giftedName);
+                        inerpoeple.Remove(gifterName);
+                        inerpoeple.Remove(giftedName);
                         dictionary.Add(gifterName, giftedName);
                         dictionary.Add(giftedName, gifterName);
                         break;
