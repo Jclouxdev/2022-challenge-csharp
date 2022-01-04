92dfd47c0883d205bd8375e04c2c8f0bd315daba
Jdev <joseph.clx@gmail.com>
2022-01-04 09:50:59 +0100

    PointOfInterest

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -4,5 +4,16 @@ namespace CSharpDiscovery.Quest03 {
         public double Longitude { get; set; }
         public string Name { get; set; }
         public static string GoogleMapsUrlTemplate { get; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+
+        public PointOfInterest() {
+            Name = "Bordeaux Ynov Campus";
+            Latitude = 44.854186;
+            Longitude = -0.5663056;
+        }
+        public PointOfInterest(string m_name, double m_latitude, double m_longitude) {
+            Latitude = m_latitude;
+            Longitude = m_longitude;
+            Name = m_name;
+        }
     }
 }
