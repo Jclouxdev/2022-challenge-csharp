d837dcd64a51a8e63831650e68b2c9b035ede84d
Jdev <joseph.clx@gmail.com>
2022-01-04 09:18:41 +0100

    Commit

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -3,6 +3,11 @@ namespace CSharpDiscovery.Quest03 {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public string Name { get; set; }
+        //public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
         public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+        public static string GetGoogleMapsUrlTemplate()
+        {
+          return GoogleMapsUrlTemplate;
+        }
     }
 }
