34b5c1f46c3154a41946ff07d27fe505c4aadbd2
Jdev <joseph.clx@gmail.com>
2022-01-10 10:20:27 +0100

    CarInterfaces

---
+++ CarInterfaces.cs
@@ -0,0 +1,20 @@
+namespace CSharpDiscovery.Quest04 {
+    public interface IThermalCar {
+        public int FuelLevel {get; set;}
+
+        public void FillUp() {}
+
+        public int GetFuelLevel() {
+            return FuelLevel;
+        }
+    }
+
+    public interface IElectricCar {
+        public int BatteryLevel {get; set;}
+        public void Recharge() {}
+
+        public int GetBatteryLevel() {
+            return BatteryLevel;
+        }
+    }
+}
